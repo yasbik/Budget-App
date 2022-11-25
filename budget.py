f07f6f060b4fdf832be2ccccdd24babd07f1508c
Abu Yasin Sabik <yasin00sabik@gmail.com>
2022-11-24 23:18:55 -0500

    Implemented bar chart

--- budget.py
+++ budget.py
@@ -51,8 +51,6 @@ class Category:
             title += "*"
         
         for item in self.ledger:
-            # transaction_description += str(item["description"]) + "\t"
-            # transaction_amount += str(item["amount"]) + "\n"
             transaction += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
 
             total += item["amount"]
@@ -60,8 +58,63 @@ class Category:
 
         return title + "\n" + transaction + "Total: " + str(total)
     
+def get_withdrawals(self):
+    total = 0
+    for item in self.ledger:
+        if item["amount"] < 0:
+            total += item["amount"]
+    return total
+
+def truncate(n):
+    multiplier = 10
+    return int(n * multiplier) / multiplier
+
+def getTotals(categories):
+    total = 0
+    breakdown = []
+    for category in categories:
+        total += category.get_withdrawals()
+        breakdown.append(category.get_withdrawals())
+    rounded = list(map(lambda x: truncate(x/total), breakdown))
+    return rounded
 
 
 
+def create_spend_chart(categories):
+    res = "Percentage spent by category\n"
+    i = 100
+    totals = getTotals(categories)
+    while i >= 0:
+        cat_spaces = " "
+        for total in totals:
+            if total * 100 >= i:
+                cat_spaces += "o  "
+            else:
+                cat_spaces = "   "
+        res += str(i).rjust(3) + "|" + cat_spaces + ("\n")
+        i -= 10
 
-# def create_spend_chart(categories):
+    dashes = "-" + "---" * len(categories)
+    names = []
+    x_axis = ""
+    for category in categories:
+        names.append(category.name)
+    
+    maxi = max(names, key = len)
+
+    for x in range(len(maxi)):
+        nameStr = "     "
+        for name in names:
+            if x >= len(name):
+                nameStr += "   "
+            else:
+                nameStr += name[x] + "   "
+        
+        if(x != len(maxi) - 1):
+            nameStr += "\n"
+
+        x_axis += nameStr
+    
+    res += dashes.rjust(len(dashes)+4) + "\n" + x_axis
+
+    return res
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-# from budget import create_spend_chart
+from budget import create_spend_chart
 from unittest import main
 
 food = budget.Category("Food")
@@ -19,7 +19,7 @@ auto.withdraw(15)
 print(food)
 print(clothing)
 
-# print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([food, clothing, auto]))
 
 # # Run unit tests automatically
 # main(module='test_module', exit=False)
